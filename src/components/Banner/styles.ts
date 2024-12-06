5784a365a0759cb77e5c21778d584113976b75dd
lucas menezes <menezeslucas24082002@gmail.com>
2024-12-06 10:01:17 -0300

    Criando o banner - 35.3

---
+++ src/components/Banner/styles.ts
@@ -0,0 +1,21 @@
+import { styled } from 'styled-components'
+
+export const Imagem = styled.div`
+  height: 560px;
+  width: 100%;
+  display: block;
+  background-repeat: no-repeat;
+  background-size: cover;
+  font-weight: bold;
+  padding-top: 340px;
+`
+
+export const Titulo = styled.h2`
+  font-size: 36px;
+  max-width: 450px;
+`
+
+export const Precos = styled.p`
+  font-size: 24px;
+  margin-top: 24px;
+`
--- src/styles.ts
+++ src/styles.ts
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
 
 export const cores = {
   branco: '#EEEEEE',
@@ -21,10 +21,10 @@ export const GlobalCss = createGlobalStyle`
     color: ${cores.branco};
     margin-top: 40px;
   }
-`
 
-export const Container = styled.div`
-  max-width: 1024px;
-  margin: 0 auto;
-  width: 100%;
+  .container {
+    max-width: 1024px;
+    margin: 0 auto;
+    width: 100%;
+  }
 `

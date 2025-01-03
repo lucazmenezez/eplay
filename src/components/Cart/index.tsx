ed1ae7a248a023057ee1bb8617eee47313bfe4f9
lucas menezes <menezeslucas24082002@gmail.com>
2025-01-03 09:03:03 -0300

    Adicionando e removendo produtos do carrinho - 37.4

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -8,45 +8,62 @@ import {
   Sidebar
 } from './styles'
 
-import image from '../../assets/images/diablo.png'
 import Tag from '../Tag'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootReducer } from '../../store'
 
-const Cart = () => (
-  <CartContainer>
-    <Overlay />
-    <Sidebar>
-      <ul>
-        <CartItem>
-          <img src={image} />
-          <div>
-            <h3>nome do jogo</h3>
-            <Tag>RPG</Tag>
-            <Tag>PS5</Tag>
-            <span>R$ 150,00</span>
-          </div>
-          <button type="button" />
-        </CartItem>
-        <CartItem>
-          <img src={image} />
-          <div>
-            <h3>nome do jogo</h3>
-            <Tag>RPG</Tag>
-            <Tag>PS5</Tag>
-            <span>R$ 150,00</span>
-          </div>
-          <button type="button" />
-        </CartItem>
-      </ul>
-      <Quantity>2 jogo(s) no carrinho</Quantity>
-      <Prices>
-        Total de R$ 381,80
-        <span>em até 6x sem juros</span>
-      </Prices>
-      <Button type="button" title="Clique aqui para continuar a compra">
-        Continuar com a compra
-      </Button>
-    </Sidebar>
-  </CartContainer>
-)
+import { close, remove } from '../../store/reducers/cart'
+import { formataPreco } from '../ProductsList'
+
+const Cart = () => {
+  const { isOpen, items } = useSelector((state: RootReducer) => state.cart)
+
+  const dispatch = useDispatch()
+
+  const closeCart = () => {
+    dispatch(close())
+  }
+
+  const getTotalPrice = () => {
+    return items.reduce((acumulador, valorAtual) => {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      return (acumulador += valorAtual.prices.current!)
+    }, 0)
+  }
+
+  const removeItem = (id: number) => {
+    dispatch(remove(id))
+  }
+
+  return (
+    <CartContainer className={isOpen ? 'is-open' : ''}>
+      <Overlay onClick={closeCart} />
+      <Sidebar>
+        <ul>
+          {items.map((item) => (
+            <CartItem key={item.id}>
+              <img src={item.media.thumbnail} alt={item.name} />
+              <div>
+                <h3>{item.name}</h3>
+                <Tag>{item.details.category}</Tag>
+                <Tag>{item.details.system}</Tag>
+                <span>{formataPreco(item.prices.current)}</span>
+              </div>
+              <button type="button" onClick={() => removeItem(item.id)} />
+            </CartItem>
+          ))}
+        </ul>
+        <Quantity>{items.length} jogo(s) no carrinho</Quantity>
+        <Prices>
+          Total de {formataPreco(getTotalPrice())}
+          <span>em até 6x sem juros</span>
+        </Prices>
+        <Button type="button" title="Clique aqui para continuar a compra">
+          Continuar com a compra
+        </Button>
+      </Sidebar>
+    </CartContainer>
+  )
+}
 
 export default Cart
